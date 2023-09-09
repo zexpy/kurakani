0d680d7b34c77f4d599db547964600b3dd87ec10
morphhyy <imbibek05@gmail.com>
2023-09-09 15:16:02 +0545

    Add trpc

---
+++ apps/server/src/lib/trpc.ts
@@ -0,0 +1,17 @@
+import { inferAsyncReturnType, initTRPC } from '@trpc/server'
+import * as trpcExpress from '@trpc/server/adapters/express'
+
+const createContext = ({
+    req,
+    res,
+}: trpcExpress.CreateExpressContextOptions) => ({}) // no context
+type Context = inferAsyncReturnType<typeof createContext>
+
+const t = initTRPC.context<Context>().create()
+
+const middleware = t.middleware
+const router = t.router
+const publicProcedure = t.procedure
+const mergerRouters = t.mergeRouters
+
+export { createContext, middleware, router, publicProcedure, mergerRouters }
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,11 +1,20 @@
 import express from 'express'
 import cors from 'cors'
+import { createContext } from './lib/trpc'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { appRouter } from './router'
 
 export const createServer = () => {
     const app = express()
-
     app.use(cors())
     app.use(express.json())
+    app.use(
+        '/trpc',
+        trpcExpress.createExpressMiddleware({
+            router: appRouter,
+            createContext,
+        })
+    )
 
     return app
 }
