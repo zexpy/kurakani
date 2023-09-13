f02f1305bf55ef924c00a9c3dc39c17d4238d1f1
morphhyy <imbibek05@gmail.com>
2023-09-14 01:06:10 +0545

    DONE

---
+++ apps/native/src/screens/OnBoarding.tsx
@@ -0,0 +1,38 @@
+import { Image } from 'react-native'
+import Onboarding from 'react-native-onboarding-swiper'
+
+const OnBoarding = ({ navigation }) => {
+    return (
+        <Onboarding
+            onSkip={() => navigation.replace('login')}
+            onDone={() => navigation.navigate('login')}
+            pages={[
+                {
+                    backgroundColor: '#3d9996',
+                    image: (
+                        <Image
+                            source={require('../../assets/onboarding_1.png')}
+                            className="h-60 w-60"
+                        />
+                    ),
+                    title: 'EXPLORE',
+                    subtitle: 'Embrace a new way to connect with the world',
+                },
+                {
+                    backgroundColor: '#e9bcbe',
+                    image: (
+                        <Image
+                            source={require('../../assets/onboarding_2.png')}
+                            className="h-60 w-60"
+                        />
+                    ),
+                    title: 'FRIENDS AWAIT!',
+                    subtitle:
+                        'Connecting you to the world, one friend at a time',
+                },
+            ]}
+        />
+    )
+}
+
+export default OnBoarding
