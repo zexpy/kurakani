1d4815a58584a0fb0c8476c35c9b2d1205707286
morphhyy <imbibek05@gmail.com>
2023-09-15 11:02:36 +0545

    Types for Trpc Inputs and Outputs

--- apps/native/src/types/user.d.ts
+++ apps/native/src/types/user.d.ts
@@ -1,5 +1,5 @@
-interface IUser {
-    username?: string
-    fullName: string
-    avatar?: string
-}
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "@kurakani/server";
+
+type RouterInput = inferRouterInputs<AppRouter>;
+type RouterOutput = inferRouterOutputs<AppRouter>;
