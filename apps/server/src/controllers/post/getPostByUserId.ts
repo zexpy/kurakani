5da1883220a6c6775a1d0bba8ae602fc055ba9d9
morphhyy <imbibek05@gmail.com>
2023-09-19 01:29:34 +0545

    sorting

--- apps/server/src/controllers/post/getPostByUserId.ts
+++ apps/server/src/controllers/post/getPostByUserId.ts
@@ -1,11 +1,13 @@
-import { z } from 'zod'
-import PostModel from '../../models/post.schema'
-import { privateProcedure } from '../../libs/trpc'
+import { z } from "zod"
+import PostModel from "../../models/post.schema"
+import { privateProcedure } from "../../libs/trpc"
 
-export const getPostByUserId = privateProcedure
-    .input(z.string())
-    .query(async opts => {
-        return await PostModel.find({ userId: opts.input })
-            .populate('userId')
-            .populate({ path: 'comments', select: ['content', 'userId'] })
-    })
+export const getPostByUserId = privateProcedure.input(z.string()).query(async (opts) => {
+    return await PostModel.find({ user_id: opts.input })
+        .populate({
+            path: "comments",
+            select: ["content", "userId"],
+        })
+        .sort({ createdAt: -1 })
+        .exec()
+})
