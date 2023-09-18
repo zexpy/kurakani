359f55acb8053ebc5564f8a3f4efeecb41cdab44
morphhyy <imbibek05@gmail.com>
2023-09-18 16:02:54 +0545

    GOTO

--- apps/native/src/screens/Profile.tsx
+++ apps/native/src/screens/Profile.tsx
@@ -1,13 +1,57 @@
-import { Text, View } from 'react-native'
-import Box from '@components/Box'
-import { setToken } from '@libs/api'
+import { Image, Text } from "react-native"
+import Box from "@components/Box"
+import { useCurrentUser } from "@hooks/useCurrentUser"
+import { MapPinIcon } from "react-native-heroicons/outline"
+import { useState } from "react"
+import { View, useWindowDimensions } from "react-native"
+import { TabView, SceneMap } from "react-native-tab-view"
+const FirstRoute = () => <View style={{ flex: 1, backgroundColor: "#ff4081" }} />
+
+const SecondRoute = () => <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
+
+const ThirdRoute = () => <View style={{ flex: 1, backgroundColor: "#727281" }} />
+
+const renderScene = SceneMap({
+    first: FirstRoute,
+    second: SecondRoute,
+    third: ThirdRoute,
+})
 
 export default function Profile() {
-    // const { isError, data, isLoading, error } = trpc.getUser.useQuery()
+    const { user } = useCurrentUser()
+    const layout = useWindowDimensions()
 
+    const [index, setIndex] = useState(0)
+    const [routes] = useState([
+        { key: "first", title: "Posts" },
+        { key: "second", title: "Photos" },
+        { key: "third", title: "Friends" },
+    ])
     return (
-        <View className="flex-1 justify-center items-center">
-            <Text className="text-red-500">Hello</Text>
-        </View>
+        <>
+            <View className="flex-row items-center gap-3 py-2">
+                <Image
+                    source={{
+                        uri: user.profile_pic,
+                    }}
+                    className="h-20 w-20 rounded-full"
+                />
+                <View>
+                    <Text className="text-xl font-bold">{user.fullName}</Text>
+                    <Text className="text-grayish">@{user.username}</Text>
+                    <View className="flex-row items-center pt-[1px]">
+                        <MapPinIcon size={15} color="black" />
+                        <Text>{user.address}</Text>
+                    </View>
+                </View>
+            </View>
+            <TabView
+                navigationState={{ index, routes }}
+                renderScene={renderScene}
+                onIndexChange={setIndex}
+                animationEnabled={true}
+                initialLayout={{ width: layout.width }}
+            />
+        </>
     )
 }
--- apps/server/src/models/user.schema.ts
+++ apps/server/src/models/user.schema.ts
@@ -1,46 +1,47 @@
-import mongoose, { InferSchemaType, Types } from "mongoose";
-import bcrypt from "bcrypt";
+import mongoose, { InferSchemaType, Types } from "mongoose"
+import bcrypt from "bcrypt"
 
 const userSchema = new mongoose.Schema(
-  {
-    firstName: String,
-    lastName: String,
-    fullName: String,
-    email: {
-      type: String,
-      required: true,
+    {
+        firstName: String,
+        lastName: String,
+        fullName: String,
+        email: {
+            type: String,
+            required: true,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            select: false,
+        },
+        address: String,
+        profile_pic: String,
+        friends: [
+            {
+                type: Types.ObjectId,
+                ref: "User",
+            },
+        ],
     },
-    username: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      select: false,
-    },
-    profile_pic: String,
-    friends: [
-      {
-        type: Types.ObjectId,
-        ref: "User",
-      },
-    ],
-  },
-  { timestamps: true }
-);
+    { timestamps: true },
+)
 
-type User = InferSchemaType<typeof userSchema>;
+type User = InferSchemaType<typeof userSchema>
 
-userSchema.pre("save", async function () {
-  const hashPassword = await bcrypt.hash(this.password, 10);
-  this.password = hashPassword;
-});
+userSchema.pre("save", async function() {
+    const hashPassword = await bcrypt.hash(this.password, 10)
+    this.password = hashPassword
+})
 
-userSchema.methods.isValidPassword = async function (password: string) {
-  return await bcrypt.compare(password, this.password);
-};
+userSchema.methods.isValidPassword = async function(password: string) {
+    return await bcrypt.compare(password, this.password)
+}
 
-const UserModel = mongoose.model<User>("User", userSchema);
+const UserModel = mongoose.model<User>("User", userSchema)
 
-export default UserModel;
+export default UserModel
