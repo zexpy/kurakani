8e407d0dec844546541359a6a3d289f4d4084770
morphhyy <imbibek05@gmail.com>
2023-09-29 22:14:12 +0545

    small fix

--- apps/server/src/controllers/friend/checkRequest.ts
+++ apps/server/src/controllers/friend/checkRequest.ts
@@ -1,28 +1,28 @@
-import { z } from "zod";
-import { privateProcedure } from "../../libs/trpc";
-import FriendShipModel from "../../models/friendship.schema";
-import { TRPCError } from "@trpc/server";
+import { z } from "zod"
+import { privateProcedure } from "../../libs/trpc"
+import FriendShipModel from "../../models/friendship.schema"
 
 export const checkRequest = privateProcedure
     .input(
         z.object({
             sender_id: z.string(),
             receiver_id: z.string(),
-        })
+        }),
     )
     .query(async ({ input }) => {
         const friendShip = await FriendShipModel.findOne({
             sender_id: input.sender_id,
             receiver_id: input.receiver_id,
             status: { $ne: "accepted" },
-        });
+        })
 
         if (!friendShip) {
-            throw new TRPCError({
+            /* throw new TRPCError({
                 code: "NOT_FOUND",
                 message: "NOT FOUND",
-            });
+            }) */
+            return null
         }
 
-        return { status: friendShip.status };
-    });
+        return { status: friendShip.status }
+    })
