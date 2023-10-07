31539ea05fd8a841823a02df130434886a20acda
morphhyy <imbibek05@gmail.com>
2023-10-08 02:17:43 +0545

    helper function

--- apps/native/src/helper/user.ts
+++ apps/native/src/helper/user.ts
@@ -1,2 +1,4 @@
 export const getSender = (logUser: any, users: any) =>
     users[0]?._id === logUser._id ? users[1] : users[0]
+
+export const getReceiver = (users: any, user: any) => users.find((u: any) => u._id !== user._id)
